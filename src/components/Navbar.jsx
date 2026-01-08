547dfad3d7cb1356544f0f61889f0e425bc37ba5
Collins Otieno <otienocollins0549@gmail.com>
2026-01-08 08:48:58 +0300

    UI: Implemented brand logo and favicon assets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/" className="logo">MORE<span>.</span></Link>
+      <Link to="/" className="logo">
+        <img src="/logo.png" alt="More Classifieds" style={{ height: '40px', width: 'auto' }} />
+      </Link>
       <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/register">Register</Link>
